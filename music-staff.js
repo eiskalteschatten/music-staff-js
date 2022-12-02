7c5617065e803fd350b62d4f95908ad3c8a0f80b
Alex Seifert <alex@alexseifert.com>
2022-12-02 14:36:29 +0100

    Draw the staff

--- music-staff.js
+++ music-staff.js
@@ -19,61 +19,51 @@ function drawTimeline() {
   let timeLineX = 0;
 
   const distanceBetweenLines = 50;
-  const howOftenYearsAreShown = 4;
 
-  // Year increment
-  const minIncrement = 25;
-  const maxIncrement = 50;
-  const yearIncrement = Math.floor(Math.random() * (maxIncrement - minIncrement + 1) + minIncrement);
+  function drawStaffLines() {
+    const numberOfLines = 5;
+    const distanceBetweenLines = canvas.height / numberOfLines;
+    const lineHeightOffset = 1;
 
-  // Starting year
-  const minStartingYear = -300;
-  const maxStartingYear = 200;
-  const maxNumberOfYears = 300;
-  const startingYear = Math.floor(Math.random() * (maxStartingYear - minStartingYear + 1) + minStartingYear);
-
-  const years = [startingYear];
-
-  for (let year = startingYear + yearIncrement; years.length < maxNumberOfYears; year += yearIncrement) {
-    years.push(year);
-  }
-
-  function drawCenterLine() {
-    ctx.beginPath();
-    ctx.setTransform(1, 0, 0, 1, 0, canvas.height / 2);
-    ctx.moveTo(0, 0);
-    ctx.lineTo(canvas.width, 0);
-    ctx.stroke();
-    ctx.closePath();
-  }
-
-  function drawYears() {
-    let yearIndex = 0;
-
-    for (let line = 1; yearIndex < years.length; line++) {
-      const hasYear = !(line % howOftenYearsAreShown);
-      const height = hasYear ? (canvas.height / 2) - fontSize : canvas.height / 2;
-      const x = (line * distanceBetweenLines) - timeLineX;
-      const y = hasYear ? (height / 2) + (fontSize / 2) : height / 2;
+    for (let line = 0; line < numberOfLines; line++) {
+      const y = line * distanceBetweenLines + lineHeightOffset;
 
       ctx.beginPath();
-      ctx.setTransform(1,0,0,1, x, y);
+      ctx.setTransform(1, 0, 0, 1, 0, y);
       ctx.moveTo(0, 0);
-      ctx.lineTo(0, height);
+      ctx.lineTo(canvas.width, 0);
       ctx.stroke();
       ctx.closePath();
-
-      if (hasYear) {
-        const year = years[yearIndex];
-        const label = year < 0 ? 'BCE' : 'CE';
-        ctx.setTransform(1,0,0,1, x, y);
-        ctx.textAlign = 'center';
-        ctx.fillStyle = foregroundColor;
-        ctx.fillText(`${Math.abs(year)} ${label}`, 0, 55);
-        yearIndex++;
-      }
     }
   }
+
+  // function drawYears() {
+  //   let yearIndex = 0;
+
+  //   for (let line = 1; yearIndex < years.length; line++) {
+  //     const hasYear = !(line % howOftenYearsAreShown);
+  //     const height = hasYear ? (canvas.height / 2) - fontSize : canvas.height / 2;
+  //     const x = (line * distanceBetweenLines) - timeLineX;
+  //     const y = hasYear ? (height / 2) + (fontSize / 2) : height / 2;
+
+  //     ctx.beginPath();
+  //     ctx.setTransform(1,0,0,1, x, y);
+  //     ctx.moveTo(0, 0);
+  //     ctx.lineTo(0, height);
+  //     ctx.stroke();
+  //     ctx.closePath();
+
+  //     if (hasYear) {
+  //       const year = years[yearIndex];
+  //       const label = year < 0 ? 'BCE' : 'CE';
+  //       ctx.setTransform(1,0,0,1, x, y);
+  //       ctx.textAlign = 'center';
+  //       ctx.fillStyle = foregroundColor;
+  //       ctx.fillText(`${Math.abs(year)} ${label}`, 0, 55);
+  //       yearIndex++;
+  //     }
+  //   }
+  // }
 
   function drawGradient() {
     const transparentColor = isDarkMode ? 'rgba(0,0,0,0)' : 'rgba(255,255,255,0)';
@@ -93,8 +83,8 @@ function drawTimeline() {
 
   function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawYears();
-    drawCenterLine();
+    // drawYears();
+    drawStaffLines();
     drawGradient();
     timeLineX += 1;
     raf = window.requestAnimationFrame(animate);
